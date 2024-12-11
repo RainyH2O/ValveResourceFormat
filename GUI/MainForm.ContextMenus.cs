cf74d10ee5dedc28323f68f985db5dfea366e064
RainyH2O <RainyH2O@users.noreply.github.com>
2024-12-12 05:11:42 +0800

    Add export entities for tab

--- GUI/MainForm.ContextMenus.cs
+++ GUI/MainForm.ContextMenus.cs
@@ -471,6 +471,9 @@ namespace GUI
             {
                 ExportFile.ExportEntitiesFromTreeNode((IBetterBaseItem)tree.SelectedNode, tree.VrfGuiContext);
             }
+            else if (owner is TabControl)
+            {
+            }
         }
     }
 }
